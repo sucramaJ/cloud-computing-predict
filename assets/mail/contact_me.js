bee41bac20fcfe08b3ac7666cfbfaef2ff26bcfc
sucramaJ <jamarcus.snyders@gmail.com>
2022-10-20 12:25:29 +0200

    Edited API endpoint

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://3dnvqp6867.execute-api.eu-west-1.amazonaws.com/default/cc-predict-js",
+                url: "https://3dnvqp6867.execute-api.eu-west-1.amazonaws.com/default",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
