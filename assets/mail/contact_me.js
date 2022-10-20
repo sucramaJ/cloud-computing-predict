0d7a755df781ef4212f89b51396e1e0c2a305059
sucramaJ <jamarcus.snyders@gmail.com>
2022-10-20 10:24:26 +0200

    Re-re-added personal API endpoint

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://yuwprx8hvj.execute-api.eu-west-1.amazonaws.com",
+                url: "https://1vo75mk4kl.execute-api.eu-west-1.amazonaws.com/default/cc-predict-js",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
