51d6b8649c81e23cf590b83e3df78cd3f7e3b4b4
sucramaJ <jamarcus.snyders@gmail.com>
2022-10-20 12:37:26 +0200

    New Lambda function api endpoint

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://3dnvqp6867.execute-api.eu-west-1.amazonaws.com/default",
+                url: "https://q1n4zhp0ok.execute-api.eu-west-1.amazonaws.com/default/cc-predict-lambda-js",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
